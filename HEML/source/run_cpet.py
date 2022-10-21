6fe8aa17928699d567a0bd8dbad49571fef40b6f
Santiago Vargas <santiagovargas@pop-os.localdomain>
2022-10-21 01:41:40 -0400

    ref updated

--- HEML/source/run_cpet.py
+++ HEML/source/run_cpet.py
@@ -4,17 +4,24 @@ from random import sample, choice
 
 def main():
     
-    #files_target = glob('../../data/cpet/options_field*.txt')
+    #files_target = glob("/ocean/projects/che160019p/santi92/cpet/options_topology*.txt")
+    #files_done = os.listdir("/ocean/projects/che160019p/santi92/cpet/")
+    #charges_folder = os.listdir("/ocean/projects/che160019p/santi92/charges_processed/")
+
     files_target = glob('../../data/cpet/options_topology*.txt')
     files_done = os.listdir("../../data/cpet/")
-    
+    charges_folder = "../../data/charge_processed/"
+
     for i in range(100):
         file = choice(files_target)
         protein=file.split("_")[-1].split(".")[0]#.split("_")[-1]
         print("protein file: {}".format(protein))
         if(protein+".top" not in files_done):
-            #print("deez")    
-            os.system("./cpet -p {} -t 16 -o {} ".format('../../data/charge_processed/{}.pqr'.format(protein), file))
+            
+            launch_str = "./cpet -p {} -t 16 -o {} ".format('{}.pqr'.format(charges_folder+protein), file)
+            print(launch_str)    
+            os.system(launch_str)
+
         print("cpet done running")
         #os.system("mv {}_0.top ../../data/cpet/{}.top".format(protein, protein))
     print("done running cpet")
