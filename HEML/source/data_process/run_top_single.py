eda9ea7d63c8876ef7471b6c886c66bdb1c3b125
Santiago Vargas <santiagovargas@pop-os.localdomain>
2022-11-02 08:33:30 -0700

    topo file runs

--- HEML/source/data_process/run_top_single.py
+++ HEML/source/data_process/run_top_single.py
@@ -1,5 +1,5 @@
 from HEML.utils.cpet import run_topology_calcs
 
 run_topology_calcs(
-    "/ocean/projects/che160019p/santi92/cpet/", 
-    "/ocean/projects/che160019p/santi92/processed_charges/")
+    "/ocean/projects/che160019p/santi92/protein_field/cpet/", 
+    "/ocean/projects/che160019p/santi92/protein_field/processed_charges/")
