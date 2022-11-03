58f7e857245df5bd021dd968bd78cbf08daa67f2
Santiago Vargas <santiagovargas@pop-os.localdomain>
2022-11-03 12:34:51 -0700

    update box topo

--- HEML/source/data_process/run_cpet_box_calc.py
+++ HEML/source/data_process/run_cpet_box_calc.py
@@ -1,5 +1,10 @@
 from HEML.utils.cpet import run_box_calcs
 
+#run_box_calcs(
+#    "/protein_field/cpet/", 
+#    "/ocean/projects/che160019p/santi92/protein_field/processed_charges/")
+
+
 run_box_calcs(
-    "/ocean/projects/che160019p/santi92/protein_field/cpet/", 
-    "/ocean/projects/che160019p/santi92/protein_field/processed_charges/")
+    "../../../data/cpet/", 
+    "../../../data/processed_charges/")
--- HEML/source/data_process/run_top_single.py
+++ HEML/source/data_process/run_top_single.py
@@ -1,5 +1,9 @@
 from HEML.utils.cpet import run_topology_calcs
 
+#run_topology_calcs(
+#    "/ocean/projects/che160019p/santi92/protein_field/cpet/", 
+#    "/ocean/projects/che160019p/santi92/protein_field/processed_charges/")
+
 run_topology_calcs(
-    "/ocean/projects/che160019p/santi92/protein_field/cpet/", 
-    "/ocean/projects/che160019p/santi92/protein_field/processed_charges/")
+    "../../../data/cpet/", 
+    "../../../data/processed_charges/")
