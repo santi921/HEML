3eec0bcbed501f0c3dcb3982221b06bd89fe6e56
Santiago Vargas <santiagovargas921@gmail.com>
2022-12-22 09:49:39 -0500

    update refs

--- HEML/source/data_process/phd3_process.py
+++ HEML/source/data_process/phd3_process.py
@@ -10,11 +10,12 @@ def main():
     root = options["compressed_proteins_folder"]
 
     # get subfolders in current directory
-    for protein_name in os.listdir():
-        if(os.path.isdir(protein_name)):
+    for protein_name in os.listdir(root):
+        if(os.path.isdir(os.path.join(root,protein_name))):
             print(protein_name)
-
-            folder_name = "./" + protein_name
+            
+            folder_name = os.path.join(root, protein_name)
+            
             #check if pdb is in folder
             if os.path.exists(os.path.join(folder_name, protein_name + ".pdb")):
                 pdb_file = protein_name + ".pdb"
@@ -33,7 +34,7 @@ def main():
 
 
             #convert xyz to pdb
-            os.system("obabel -i xyz {} -o pdb -O ./{}/{}_heme.pdb". format(xyz_file_name, protein_name, protein_name))
+            os.system("obabel -i xyz {} -o pdb -O {}/{}_heme.pdb". format(os.path.join(folder_name, xyz_file_name), folder_name, protein_name))
 
 
             # write with =OH
@@ -42,14 +43,17 @@ def main():
             # write xyz to file - write xyz
             xyz_file_name = write_dict_to_xyz(folder_name, protein_name, dict_xyz, add_o = False, add_oh = True)
             #convert xyz to pdb
-            os.system("obabel -i xyz {} -o pdb -O ./{}/{}_heme.pdb". format(xyz_file_name, protein_name, protein_name + "_oh"))
+
+            os.system("obabel -i xyz {} -o pdb -O {}/{}_heme.pdb". format(os.path.join(folder_name, xyz_file_name), folder_name, protein_name+ "_oh"))
 
             # write vanilla
             # pull hemes and ligands from pdb - dict of xyz subset
             dict_xyz = extract_heme_and_ligand_from_pdb(folder_name, pdb_file, add_o = False, add_oh = False)
             # write xyz to file - write xyz
-            xyz_file_name = write_dict_to_xyz(folder_name, protein_name, dict_xyz, add_oh = False, add_o = False)            
+            xyz_file_name = write_dict_to_xyz(folder_name, protein_name, dict_xyz, add_o = False, add_oh = False)            
             #convert xyz to pdb
-            os.system("obabel -i xyz {} -o pdb -O ./{}/{}_heme.pdb". format(xyz_file_name, protein_name, protein_name + "_o"))
+            os.system("obabel -i xyz {} -o pdb -O {}/{}_heme.pdb". format(os.path.join(folder_name, xyz_file_name), folder_name, protein_name+ "_o"))
+
+            #os.system("obabel -i xyz {} -o pdb -O ./{}/{}_heme.pdb". format(xyz_file_name, protein_name, protein_name + "_o"))
 
 main()
