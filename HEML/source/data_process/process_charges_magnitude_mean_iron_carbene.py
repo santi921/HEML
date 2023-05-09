0a3bb2da52daa637eb340a7245abb12f0f7d6a83
Santiago Vargas <santiagovargas921@gmail.com>
2023-05-09 10:17:26 -0700

    jfc

--- HEML/source/data_process/process_charges_magnitude_mean_iron_carbene.py
+++ HEML/source/data_process/process_charges_magnitude_mean_iron_carbene.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os, re, argparse
 from glob import glob
-from HEML.utils.data import get_options, check_if_file_is_empty, get_fe_positions
+from HEML.utils.data import get_options, check_if_file_is_empty, get_fe_positions, get_c1_positions
 from HEML.utils.mol2topqr import mol2_to_pqr_folder
 
 if __name__ == "__main__":
